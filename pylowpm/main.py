9f0b1fd1dcc86833fe442e6f2ec4ad8eedcf38b1
LucasZapico <hello@lucaszapico.space>
2024-12-09 10:53:09 -0800

    feat: init main

---
+++ pylowpm/main.py
@@ -0,0 +1,10 @@
+from utils.pretty_cli_logger import print_info 
+from utils.logger import logger
+
+def main():
+    print_info("Hello world")
+    logger.info("Hello from info logger world")
+
+
+if __name__ == "__main__":
+    main()
