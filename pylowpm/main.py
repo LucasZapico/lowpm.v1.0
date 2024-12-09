1d6199616d5b7b3a7401ba8c52a87decec28cd5c
LucasZapico <hello@lucaszapico.space>
2024-12-09 11:13:42 -0800

    feat: init main

--- pylowpm/main.py
+++ pylowpm/main.py
@@ -2,6 +2,7 @@ from utils.pretty_cli_logger import print_info
 from utils.logger import logger
 
 def main():
+    """Main function."""
     print_info("Hello world")
     logger.info("Hello from info logger world")
 
